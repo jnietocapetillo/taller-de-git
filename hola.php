add12dbc4a68913d9e64aa724ac97d04c97f8e46
Javier Nieto <capeniet@gmail.com>
2021-05-25 10:48:42 +0200

    agregado autor del proyecto y su correo

--- hola.php
+++ hola.php
@@ -1,4 +1,6 @@
 <?php
+    //el autor es Javier Nieto
+    //email: capeniet@gmail.com
     // si no hay argumento, el valor es Javier Nieto
     $nombre = isset($argv[1]) ? $argv[1] : "Javier Nieto";
     @print 'Hola, {$nombre}\n';
