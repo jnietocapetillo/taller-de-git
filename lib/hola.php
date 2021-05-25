bddf9ff57a92009d07b26c6c249800addf83dc89
Javier Nieto <capeniet@gmail.com>
2021-05-25 11:08:40 +0200

    hola utiliza la clase holaMundo

--- lib/hola.php
+++ lib/hola.php
@@ -2,7 +2,8 @@
     //el autor es Javier Nieto
     //email: capeniet@gmail.com
     // si no hay argumento, el valor es Javier Nieto
+    require('holaMundo');
     $nombre = isset($argv[1]) ? $argv[1] : "Javier Nieto";
-    @print 'Hola, {$nombre}\n';
+    print new holaMundo($nombre);
     
 ?>
