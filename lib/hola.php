7f84d476a5e35d4abeda41e0f909605a78e6c300
Javier Nieto <capeniet@gmail.com>
2021-05-25 12:22:38 +0200

    despues de conflicto

--- lib/hola.php
+++ lib/hola.php
@@ -2,7 +2,8 @@
     //el autor es Javier Nieto
     //email: capeniet@gmail.com
     // si no hay argumento, el valor es Javier Nieto
-    $nombre = isset($argv[1]) ? $argv[1] : "Mundo";
+    print "Introduce tu nombre:";
+    $nombre = trim(fgets(STDIN));
     print new HolaMundo($nombre);
     
 ?>
